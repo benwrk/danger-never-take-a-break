f45c3a7a31aed817c166cdbef541f0ab31fb3fa0
Benjapol Worakan <benwrk@live.com>
2017-12-02 06:22:10 +0000

    Add game over UI

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Car : MonoBehaviour {
+public class Car : MonoBehaviour
+{
+
+    private bool _isActive;
+    private Animator _animator;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    _isActive = true;
+	    _animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+	    if (_isActive)
+	    {
+	        if (Input.GetKey(KeyCode.W))
+	        {
+	            _animator.SetBool("IsAccelerating", true);
+	        }
+	        else
+	        {
+	            _animator.SetBool("IsAccelerating", false);
+	        }
+	    }
 	}
 }
