7805ebfd7e99c9b66f5ee136f08693f38e481c46
Benjapol Worakan <benwrk@live.com>
2017-12-03 19:00:19 +0000

    Add scrolling to objects

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public static readonly float GameTime = 150.00f;
+    public static GameController Instance;
+
+    public Text TimeText;
+    public Text ScoreText;
+
+    private float _timeLeft;
+    private float _score;
+
+    public enum GameState
+    {
+        NotStarted, Paused, Active, Over
+    }
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timeLeft -= Time.deltaTime;
+
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        
+    }
+
+    private void StartGame()
+    {
+        ResetCounters();
+    }
+
+    private void ResetCounters()
+    {
+        _timeLeft = GameTime;
+        _score = 0;
+    }
+
+    public void Restart()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/HorizontallyScollingRigidbody.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontallyScollingRigidbody : MonoBehaviour
+{
+
+    public float ScrollSpeed = 0f;
+    private Rigidbody2D _rigidBody;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _rigidBody = GetComponent<Rigidbody2D>();
+        _rigidBody.velocity = new Vector2(ScrollSpeed, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
